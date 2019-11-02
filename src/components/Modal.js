2b822b72269094c6906a3191e0fed24bc71d4f14
Shubham <shup30@gmail.com>
2019-11-02 19:14:43 +0530

    update

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -54,8 +54,9 @@ const ModalContainer = styled.div`
 position: fixed;
 top: 0;
 left: 0;
+right: 0;
 bottom: 0;
-background: rgba(0,0,0,0.3);
+background: rgba(0, 0, 0,  0.3);
 display: flex;
 align-items: center;
 justify-content:center;
@@ -64,4 +65,4 @@ justify-content:center;
     background:var(--mainWhite);
 }
 
-`
+`;
